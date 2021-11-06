822222ec64b6dfc56f60e6af89d731494bf1ca59
Andy Mukanya <61330251+amukanya@users.noreply.github.com>
2021-11-06 15:13:45 +0000

    Added menu and printed first option

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import csv
 from pprint import pprint
 
-# Initaiting values of totals Buy and Sell
+GET_SUM = 1
+GET_DATA = 2
+QUIT = 3
+
+def get_menu():
+  print()
+  print("CryptoCurrency Log")
+  print("------------------\n")
+  print(GET_SUM,". Buy, Sell, and Total Account Value Overview")
+  print(GET_DATA,". List Each Crypto Currency and it's total Value in the account")
+  print(QUIT,". To quit the program\n")
+
+  choice = int(input("Enter your option: "))
+
+  while choice < GET_SUM or choice > QUIT:
+    choice = int(input("Enter a valid option: "))
+  return choice
+
+
+
+
+# Initating values of totals Buy and Sell
 totBuy = 0
 totSell = 0
 
@@ -19,6 +40,22 @@ with open('example_log.csv', newline='') as csvfile:
             totSell += float(row['Quantity Transacted'])
     
 # Printing the results 
-    print("The total sum of all the 'Buy' transactions is:  ",totBuy)
-    
-    print("The total sum of all the 'Sell'transactions is:  ",totSell)
+    valOfAcc = totBuy - totSell
+    def get_sum():
+      print("The total sum of all the 'Buy' transactions is:  ",totBuy)
+      
+      print("The total sum of all the 'Sell'transactions is:  ",totSell)
+
+      print("The value of the account is: ",valOfAcc)
+
+
+choice = 0
+while choice != QUIT:
+  choice = get_menu()
+  if choice == GET_SUM:
+    get_sum()
+#   elif choice == GET_DATA:
+#     get_data()
+#   elif choice == QUIT:
+#     print('You have left the program.\nWe hope to see you soon!')
+
