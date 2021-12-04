3cfeb3850e6bc972d3f00876d64b3068fe852e40
Randy Mboma <61330302+rmboma1453@users.noreply.github.com>
2021-12-04 15:41:17 +0000

    Added second menu option

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import csv
 from pprint import pprint
 
+#Initiating the menu choices'values
 GET_SUM = 1
 GET_DATA = 2
 QUIT = 3
 
+the_list = []
+cryptos = {}
+#Defining function for the menu
 def get_menu():
   print()
   print("CryptoCurrency Log")
@@ -22,15 +26,15 @@ def get_menu():
 
 
 
-# Initating values of totals Buy and Sell
+#Initating values of totals Buy and Sell
 totBuy = 0
 totSell = 0
 
-# Reading the CVS file 
+#Reading the CVS file 
 with open('example_log.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
 
-# Summing transactions with 'Buy' and 'Sell' type:
+#Summing transactions with 'Buy' and 'Sell' type:
     for row in reader:
          
           if row['Transaction Type'] =='Buy':
@@ -38,24 +42,43 @@ with open('example_log.csv', newline='') as csvfile:
          
           if row['Transaction Type'] =='Sell':
             totSell += float(row['Quantity Transacted'])
-    
-# Printing the results 
+            
+          
+          data = row['Asset'] +": "+ row['Quantity Transacted']
+
+          if(row['Asset'] in cryptos.keys()):
+            cryptos[row['Asset']] += float(row['Quantity Transacted'])
+          else:
+            # print(row['Quantity Transacted'])
+            cryptos[row['Asset']] = float(row['Quantity Transacted'])
+
+          the_list.append(data)
+#Printing the results 
     valOfAcc = totBuy - totSell
-    def get_sum():
-      print("The total sum of all the 'Buy' transactions is:  ",totBuy)
-      
-      print("The total sum of all the 'Sell'transactions is:  ",totSell)
 
-      print("The value of the account is: ",valOfAcc)
+    #Defining the summing function
+def get_sum():
+  print("The total sum of all the 'Buy' transactions is:  ",totBuy)
+  
+  print("The total sum of all the 'Sell'transactions is:  ",totSell)
 
+  print("The value of the account is: ",valOfAcc)
 
+def get_data(data):
+    for line in the_list:
+      print(line)
+
+#Menu choices and output
 choice = 0
 while choice != QUIT:
   choice = get_menu()
   if choice == GET_SUM:
     get_sum()
-#   elif choice == GET_DATA:
-#     get_data()
-#   elif choice == QUIT:
-#     print('You have left the program.\nWe hope to see you soon!')
+  if choice == GET_DATA:
+    for x, y in cryptos.items():
+      print(x,": ",y)
+    # print(cryptos)
+    # get_data(data)
+  elif choice == QUIT:
+    print("You have left the program.\nWe hope to see you soon!\U0001F44B")
 
